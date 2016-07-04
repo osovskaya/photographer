e077b57d4d028d6979bdd49b0c6f4e0b83f308f4
Tatyana Osovskaya <tanya.osovskaya@mail.ru>
2016-07-04 19:40:36 +0300

    Update config.php

--- src/Console/Command/config.php
+++ src/Console/Command/config.php
@@ -3,7 +3,7 @@ return $config = array(
     'root' =>'/var/www/mysite.com/public/',
     'db' => array(
     'dsn' => "mysql:host=localhost;dbname=photographer",
-    'username' => 'root',
-    'password' => 'O$0vskaya'
+    'username' => '',
+    'password' => ''
 )
 );
